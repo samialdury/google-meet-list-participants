fe75c971d7e18b549e04bdfa55f929cab2412af4
Sami Al-Dury <57627858+samialdury@users.noreply.github.com>
2021-01-19 15:38:04 +0100

    Upload script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # google-meet-list-participants
 Simple script that you can add as a bookmark to list current participants on Google Meet.
+
+# Installation
+1. Visit [this link]('https://raw.githubusercontent.com/samialdury/google-meet-list-participants/main/script-minified-encoded.js') and copy everything.
+1. Create new bookmark with name of your choice and paste the script you just copied as its URL.
+
+# Usage
+Just click the bookmark whenever you're in call to display the output😀
---
+++ script.js
@@ -0,0 +1,43 @@
+names = [];
+temp = [];
+final = [];
+output = '';
+
+// If sidebar isn't open, click the ico to open it
+try {
+  document.querySelectorAll('.ggUFBf')[1].childNodes[1].childNodes.forEach( function (e) {
+    names.push(e.childNodes[0].childNodes[1].childNodes[0].childNodes[0].innerHTML);
+  });
+} catch {
+  document.querySelectorAll('span.azXnTb')[0].click()
+  document.querySelectorAll('.ggUFBf')[1].childNodes[1].childNodes.forEach( function (e) {
+    names.push(e.childNodes[0].childNodes[1].childNodes[0].childNodes[0].innerHTML);
+  });
+}
+
+// Remove duplicates and split them on space
+[...new Set(names)].forEach(function (v) {
+  temp.push(v.split(' '));
+});
+
+// Sort by last name alphabetically
+temp.sort(function (a, b) {
+	return a[0]-b[0];
+});
+
+// Join name back together
+temp.forEach(function (e) {
+  final.push(e.join(' '));
+});
+
+// Split users by new line
+output = final.join('\n');
+
+// Prompt with output
+window.prompt('Ctrl+C (or Cmd+C) + Enter to copy the list and close this window!\n\nhttps://github.com/samialdury/google-meet-list-participants\n\nOutput is copied with new lines.', output);
+
+// Delete declared variables so we can run the script again without reloading the page
+delete names;
+delete temp;
+delete final;
+delete output;
